520cf7ef656d1b69ec97e1b4cb19a57227c9706e
sakolin3-debug <sakolin3@gmail.com>
2026-08-05 18:30:55 -0400

    Add error handling for blank input

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def emot_detector():
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] is None:
-        return "Invalid text! Please try again."
+        return "Invalid text! Please try again!"
 
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(
         response['anger'],
