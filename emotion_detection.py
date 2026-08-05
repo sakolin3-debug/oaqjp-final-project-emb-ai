4d9a8223392dcc04285eb5aa96393f8118e5d320
sakolin3-debug <sakolin3@gmail.com>
2026-08-05 15:51:50 -0400

    Format emotion detector output and add dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,4 +9,33 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=input_json, headers=headers)
     formatted_response = json.loads(response.text)
 
-    return formatted_response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    final_output = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+
+    return final_output
+
+
